2c6310b4e2badaed886fa91832514d3dd608ffc7
raj98897 <raj98897sharma@gmail.com>
2025-09-04 13:07:49 +0530

    Add Basic02 folder with README

---
+++ Basic02/Calculator.java
@@ -0,0 +1,49 @@
+
+import java.util.Scanner;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // Input 
+        System.out.print("Enter 1st number: ");
+        double num1 = sc.nextDouble();
+
+        System.out.print("Enter 2nd number: ");
+        double num2 = sc.nextDouble();
+
+        System.out.print("Choose operation (+, -, *, /): ");
+        char op = sc.next().charAt(0);
+
+        double result;
+
+        // Perform calculation
+        switch (op) {
+            case '+':
+                result = num1 + num2;
+                System.out.println("Result = " + result);
+                break;
+            case '-':
+                result = num1 - num2;
+                System.out.println("Result = " + result);
+                break;
+            case '*':
+                result = num1 * num2;
+                System.out.println("Result = " + result);
+                break;
+            case '/':
+                if (num2 != 0) {
+                    result = num1 / num2;
+                    System.out.println("Result = " + result);
+                } else {
+                    System.out.println("Error! Division by zero.");
+                }
+                break;
+            default:
+                System.out.println("Invalid operation!");
+        }
+        sc.close();
+
+    }
+
+}
---
+++ Problem_Solving/CheckPrime.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class CheckPrime {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        boolean isPrime = true;
+
+        if (num < 2){
+            isPrime = false; // numbers less than 2 are not prime.
+        }else{
+            for(int i = 2; i*i < num;i++){
+                if(num%i==0){
+                    isPrime=false;
+                    break;
+                }
+            }
+        }
+        if(isPrime){
+            System.out.println(num +" is a prime number.");
+        }else{
+            System.out.println(num +" is not a prime number.");
+        }
+        sc.close();
+    }
+    
+}
