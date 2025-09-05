85aa4c54eb63d6508e0060c7cb9876599cadc2a8
raj98897 <raj98897sharma@gmail.com>
2025-09-05 18:51:36 +0530

    New code added

---
+++ Problem_Solving/isPalindrome.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class isPalindrome {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int original = n;
+        int rev = 0;
+        while(n>0){
+            int digit = n%10;
+            rev = rev*10+digit;
+            n/=10;
+        }
+        if(rev == original){
+            System.out.println("True");
+        }else{
+            System.out.println("False");
+        }
+    }}
+    
+
